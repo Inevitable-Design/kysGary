fa4dae956508a944663b4e191adde053702dd6d8
Azure-Pharma <sales@ourazure.in>
2024-12-24 04:10:57 +0530

    feat: added okayish looking

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -79,12 +79,17 @@ module.exports = {
           from: { height: "var(--radix-collapsible-content-height)" },
           to: { height: 0 },
         },
+        shimmer: {
+          '0%': { backgroundPosition: '200% 0' },
+          '100%': { backgroundPosition: '-200% 0' }
+        }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "collapsible-down": "collapsible-down 0.2s ease-in-out",
         "collapsible-up": "collapsible-up 0.2s ease-in-out",
+        shimmer: 'shimmer 2s ease-in-out infinite',
       },
     },
   },
